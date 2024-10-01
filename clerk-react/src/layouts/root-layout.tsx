b63d40a537b19027cf72e89025fa990cf01f8fca
Isaac Gemal <isaacgemal@gmail.com>
2024-10-01 15:27:28 -0400

    routing example

---
+++ clerk-react/src/layouts/root-layout.tsx
@@ -0,0 +1,39 @@
+import { Link, Outlet, useNavigate } from 'react-router-dom'
+import { ClerkProvider, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || "pk_test_YXdhaXRlZC1tYWNhcXVlLTkzLmNsZXJrLmFjY291bnRzLmRldiQ"
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error('Missing Publishable Key')
+}
+
+export default function RootLayout() {
+    const navigate = useNavigate()
+
+    return (
+        <ClerkProvider
+            routerPush={(to) => navigate(to)}
+            routerReplace={(to) => navigate(to, { replace: true })}
+            publishableKey={PUBLISHABLE_KEY}
+        >
+            <div>
+                <header className="header">
+                    <div>
+                        <div>
+                            <p>Clerk + React + React Router App</p>
+                        </div>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                        <SignedOut>
+                            <Link to="/sign-in">Sign In</Link>
+                        </SignedOut>
+                    </div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </ClerkProvider>
+    )
+}
